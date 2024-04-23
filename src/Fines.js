3a43acfc2b90727ac8509c93ff7b55c912a0512c
Jony Phiri <2456518@students.wits.ac.za>
2024-04-23 23:06:58 +0200

    add Fines stub

--- src/Fines.js
+++ src/Fines.js
@@ -1,6 +1,22 @@
 import React from "react";
 const Fines = () => {
     //Fetch the user's fines and store them in an array of objects
+    let userFines = [
+        {
+            id: 1,
+            admin: 2,
+            resident: 75,
+            amount: 750,
+            status: "due"
+        },
+        {
+            id: 5,
+            admin: 1,
+            resident: 75,
+            amount: 750,
+            status: "due"
+        }
+    ]
 
     return(
         <article className="Fines">
