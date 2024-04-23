c957250cb7af9661e7154e7a619c03adbc19d179
Jony Phiri <2456518@students.wits.ac.za>
2024-04-23 12:22:04 +0200

    Separate app from home

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import Home from "./Home";
 
 function App() {
-  const [data, setData] = useState('');
-
-  useEffect(() => {
-    (async function () {
-      const { text } = await( await fetch(`/api/message`)).json();
-      setData(text);
-    })();
-  });
+  
 
   return (
-    <div>{data}</div>
+    <Home/>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+const Home = () => {
+
+    const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/message`)).json();
+      setData(text);
+    })();
+  });
+
+  return (
+    <div>{data}</div>
+  );
+
+}
+
+export default Home;
