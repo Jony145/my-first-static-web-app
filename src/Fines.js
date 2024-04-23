800beaaa2bd9b50d6cc3112aa09a97b5364c4631
Jony Phiri <2456518@students.wits.ac.za>
2024-04-23 15:36:58 +0200

    add comments

--- src/Fines.js
+++ src/Fines.js
@@ -1,7 +1,12 @@
 const Fines = () => {
+    //Fetch the user's fines and store them in an array of objects
 
     return(
-        <article>
+        <article className="Fines">
+            <h1>Fines</h1>
+            
+            {/* Add all user's fines here by recursively adding file componants */}
+             
             <p>It's working </p>
         </article>
     );
