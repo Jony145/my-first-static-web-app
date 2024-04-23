1fc1ee2a762dfa9e0a5ebf32b0e1d039905ee02c
Jony Phiri <2456518@students.wits.ac.za>
2024-04-23 19:48:52 +0200

    Init React routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
 
 
   return (
-    <article className='App'>
-      <nav className='Navbar'>
-        <h1>Sectional property management App</h1>
-        <ul className='Links'>
-          <a href='/'>Home</a>
-          <a href='/Fines'>Fines</a>
-        </ul>
-      </nav>
+    <Router>
+      {/* Bellow is the app which is an article */}
+      <article className='App'> 
+      {/* this is the navbar which is always there */}
+        <nav className='Navbar'>
+          <h1>Sectional property management App</h1>
+          <ul className='Links'>
+            <a href='/'>Home</a>
+            <a href='/Fines'>Fines</a>
+          </ul>
+        </nav>
 
-      <section className='Page'><Home/></section>
-      
-    </article>
-
+        {/* Bellow is the page content */}
+        <section className='Page'>
+          <Switch>
+            <Route path='/'> 
+              <Home></Home>
+            </Route>
+          </Switch>
+        </section>
+        
+      </article>
+    </Router>
   );
 }
 
