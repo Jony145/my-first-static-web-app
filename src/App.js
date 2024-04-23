0e26d9546e8477527f509d975327beeb76cc9c0c
Jony Phiri <2456518@students.wits.ac.za>
2024-04-23 12:35:28 +0200

    Return app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import Home from "./Home";
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/message`)).json();
+      setData(text);
+    })();
+  });
 
   return (
-    <article>
-      <Home/>
-    </article>
-    
+    <div>{data}</div>
   );
 }
 
