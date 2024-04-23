3e52343fb8642681a5af16d8f6c04de72402d138
Jony Phiri <2456518@students.wits.ac.za>
2024-04-23 12:44:29 +0200

    Remove other code in App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import React, { useState, useEffect } from 'react';
+
 import Home from './Home';
 
 function App() {
-  const [data, setData] = useState('');
 
-  useEffect(() => {
-    (async function () {
-      const { text } = await( await fetch(`/api/message`)).json();
-      setData(text);
-    })();
-  });
 
   return (
     <div>
-    <div>{data}</div>
     <Home/>
     </div>
 
