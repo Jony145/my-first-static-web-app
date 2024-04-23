54d5fc1f6b4839fc2c26b117186346dc3a047716
Jony Phiri <2456518@students.wits.ac.za>
2024-04-23 20:48:08 +0200

    remove router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Home from './Home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {  Route, Routes } from 'react-router-dom';
 
 function App() {
 
